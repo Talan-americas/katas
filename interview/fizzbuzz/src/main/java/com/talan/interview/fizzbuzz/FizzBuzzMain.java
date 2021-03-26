ea891107514c5f9db4c344eafdb480c81d5d7bee
matthieu.borgraeve <matthieu.borgraeve@bell.ca>
2021-03-26 12:02:49 -0400

    result. OK

--- interview/fizzbuzz/src/main/java/com/talan/interview/fizzbuzz/FizzBuzzMain.java
+++ interview/fizzbuzz/src/main/java/com/talan/interview/fizzbuzz/FizzBuzzMain.java
@@ -1,11 +1,10 @@
 package com.talan.interview.fizzbuzz;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FizzBuzzMain {
     public static void main(String[] args) {
-
         printResult(doFizzBuzz(100));
     }
 
@@ -14,12 +13,22 @@ public class FizzBuzzMain {
         /* Return a list of elements from 0 to n included, as strings.
          * If element is even, then output "Fizz" instead
          * If element is multiple of 5, then output "Buzz" instead
-         * If element is both, then output "FizzBuzz" instead
+         * If element is both even and multiple of 5, then output "FizzBuzz" instead
          */
 
         //todo your code here
+        List<String> returnedList = new ArrayList<>();
 
-        return Collections.emptyList();
+        int count = 0;
+
+        while (count <= n) {
+            returnedList.add(count % 2 == 0 && count % 10 != 0 ? "Fizz" :
+                    (count % 5 == 0 && count % 10 != 0 ? "Buzz" :
+                            (count % 10 == 0 ? "FizzBuzz" : new Integer(count).toString())));
+            count++;
+        }
+
+        return returnedList;
     }
 
     public static void printResult(List<String> list) {
--- interview/fizzbuzz/src/test/java/com/talan/interview/fizzbuzz/FizzBuzzMainOwnerTest.java
+++ interview/fizzbuzz/src/test/java/com/talan/interview/fizzbuzz/FizzBuzzMainOwnerTest.java
@@ -1,5 +1,6 @@
 package com.talan.interview.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -19,6 +20,16 @@ class FizzBuzzMainOwnerTest {
         assertThat(result.get(index)).isEqualTo(expected);
     }
 
+    @Test
+    void doFizzBuzz_0() {
+        assertThat(FizzBuzzMain.doFizzBuzz(0)).hasSize(1).contains("FizzBuzz");
+    }
+
+    @Test
+    void doFizzBuzz_10() {
+        assertThat(FizzBuzzMain.doFizzBuzz(10)).hasSize(11);
+    }
+
     public static Stream<Arguments> doFizzBuzz() {
         return Stream.of(
                 Arguments.of(2, "Fizz"),
@@ -26,7 +37,9 @@ class FizzBuzzMainOwnerTest {
                 Arguments.of(10, "FizzBuzz"),
                 Arguments.of(1, "1"),
                 Arguments.of(7, "7"),
-                Arguments.of(99, "99")
+                Arguments.of(99, "99"),
+                Arguments.of(0, "FizzBuzz")
+
         );
     }
 
