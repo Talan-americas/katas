b4faf2468fd5cfbd34bad31edab5d6d0e7579ef8
matthieu.borgraeve <matthieu.borgraeve@bell.ca>
2021-04-09 13:03:26 -0400

    Adding requirements.

---
+++ katas/tdd/src/main/java/com/talan/kata/tdd/BowlingGame.java
@@ -0,0 +1,15 @@
+package com.talan.kata.tdd;
+
+public class BowlingGame {
+
+    /*
+     * Requirements:
+     * 1 - Count the points
+     * 2 - Display the game as String separated by pipes or any other format, like \"|3,4|5,/|X|3|\"
+     *
+     * Assume:
+     * - Only one player
+     * - All inputs will be valid (no negative score, no weird rolls, proper number of roll and frames)
+     */
+
+}
