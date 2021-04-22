ec09f5484164f6e9f52fbb1559c3442ad77043ff
matthieu.borgraeve <matthieu.borgraeve@bell.ca>
2021-04-22 12:55:56 -0400

    WIP session2

---
+++ katas/tdd/src/main/java/com/talan/kata/tdd/Frame.java
@@ -0,0 +1,44 @@
+package com.talan.kata.tdd;
+
+public class Frame {
+
+    private final State state;
+    private final Roll roll;
+    private int firstRoll, secondRoll;
+
+    public Frame() {
+        firstRoll = -1;
+        secondRoll = -1;
+        roll = Roll.NOROLL;
+        state = State.REGULAR;
+    }
+
+    public int getFirstRoll() {
+        return firstRoll;
+    }
+
+    public int getSecondRoll() {
+        return secondRoll;
+    }
+
+    public void roll(int rollInt) {
+        if (roll.equals(Roll.NOROLL)) {
+            firstRoll = rollInt;
+            roll = Roll.ROLLONE;
+        } else {
+            secondRoll = rollInt;
+        }
+    }
+
+    public boolean isSpare() {
+        return getFirstRoll() + getSecondRoll() == 10 && getFirstRoll() != -1 && getSecondRoll() != -1;
+    }
+
+    public boolean isStrike() {
+        return getFirstRoll() == 10;
+    }
+
+    public static enum Roll {NOROLL, ROLLONE, ROLLFINISHED}
+
+    public static enum State {REGULAR, SPARE, STRIKE}
+}
--- katas/tdd/src/test/java/com/talan/kata/tdd/FrameTest.java
+++ katas/tdd/src/test/java/com/talan/kata/tdd/FrameTest.java
@@ -2,13 +2,66 @@ package com.talan.kata.tdd;
 
 import org.junit.jupiter.api.Test;
 
-import java.awt.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
-public class FrameTest {
+class FrameTest {
 
     @Test
-    public void createFrame() {
-        Frame frame = new Frame(3);
+    void createFrame() {
+        Frame frame = new Frame();
         //TODO next week : what do we assert on the Frame ?
+        frame.roll(3);
+        frame.roll(4);
+        assertThat(frame.getFirstRoll())
+                .isEqualTo(3);
+        assertThat(frame.getSecondRoll())
+                .isEqualTo(4);
     }
+
+    @Test
+    void isSpare() {
+        Frame frame = new Frame();
+        frame.roll(3);
+        frame.roll(7);
+        assertThat(frame.isSpare()).isTrue();
+    }
+
+    @Test
+    void isNotSpare() {
+        Frame frame2 = new Frame();
+        frame2.roll(3);
+        frame2.roll(4);
+        assertThat(frame2.isSpare()).isFalse();
+    }
+
+    @Test
+    void isStrikeNotSpare() {
+        Frame frame3 = new Frame();
+        frame3.roll(10);
+        assertThat(frame3.isSpare()).isFalse();
+    }
+
+    @Test
+    void isStrike() {
+        Frame frame = new Frame();
+        frame.roll(10);
+        assertThat(frame.isStrike()).isTrue();
+    }
+
+    @Test
+    void isNotStrike() {
+        Frame frame2 = new Frame();
+        frame2.roll(3);
+        frame2.roll(4);
+        assertThat(frame2.isStrike()).isFalse();
+    }
+
+    @Test
+    void isSpareNotStrike() {
+        Frame frame3 = new Frame();
+        frame3.roll(3);
+        frame3.roll(7);
+        assertThat(frame3.isStrike()).isFalse();
+    }
+
 }
