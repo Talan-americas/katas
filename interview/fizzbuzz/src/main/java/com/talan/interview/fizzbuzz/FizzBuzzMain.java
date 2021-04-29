794c42f90ce40326ed04641c39338b202f94cca7
matthieu.borgraeve <matthieu.borgraeve@bell.ca>
2021-04-29 07:53:54 -0400

    WIP session2

--- interview/fizzbuzz/src/main/java/com/talan/interview/fizzbuzz/FizzBuzzMain.java
+++ interview/fizzbuzz/src/main/java/com/talan/interview/fizzbuzz/FizzBuzzMain.java
@@ -1,33 +1,35 @@
 package com.talan.interview.fizzbuzz;
 
-import java.util.ArrayList;
+import lombok.extern.log4j.Log4j2;
+
 import java.util.Collections;
 import java.util.List;
 
+@Log4j2
 public class FizzBuzzMain {
     public static void main(String[] args) {
 
         printResult(doFizzBuzz(100));
     }
 
+    /**
+     * Returns a list of elements from 0 to n included, as strings.
+     * If element is even (multiple of 2), then output "Fizz" instead
+     * If element is multiple of 5, then output "Buzz" instead
+     * If element is both even (multiple of 2) and multiple of 5, then output "FizzBuzz" instead
+     *
+     * @param n upper bound up to which compute the results
+     * @return the list of String following rules above, from 0 to n included
+     */
     public static List<String> doFizzBuzz(int n) {
 
-        /* Return a list of elements from 0 to n included, as strings.
-         * If element is even, then output "Fizz" instead
-         * If element is multiple of 5, then output "Buzz" instead
-         * If element is both, then output "FizzBuzz" instead
-         */
-
         //todo your code here
-        List<String> test = new ArrayList<>();
-
         return Collections.emptyList();
     }
 
     public static void printResult(List<String> list) {
         for (int i = 0; i < list.size(); i++) {
             System.out.println("Row " + i + ":" + list.get(i));
-
         }
     }
 }
--- interview/fizzbuzz/src/test/java/com/talan/interview/fizzbuzz/FizzBuzzMainTest.java
+++ interview/fizzbuzz/src/test/java/com/talan/interview/fizzbuzz/FizzBuzzMainTest.java
@@ -2,11 +2,22 @@ package com.talan.interview.fizzbuzz;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class FizzBuzzMainTest {
 
     @Test
     void doFizzBuzz() {
+        //todo your test here
+    }
 
-        //here your test
+    @Test
+    void demoTest() {
+        // here you can call your code with any argument:
+        FizzBuzzMain.doFizzBuzz(4);
+        // and you can use assertJ or usual junit assertions:
+        assertThat(FizzBuzzMain.doFizzBuzz(4)).isEqualTo("");
+        assertEquals(1, 1);
     }
 }
